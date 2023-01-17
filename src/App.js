e4967cd1ebea7c3c97da11bfe849e1be5a2266ea
Alex Jimenez <alexjimenezada@gmail.com>
2023-01-17 22:57:50 +0100

    Added Header, Home and Footer components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
     return (
         <div className="App">
-            <BrowserRouter></BrowserRouter>
+            <BrowserRouter>
+                <Header />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Home />}></Route>
+                    </Routes>
+                </main>
+                <Footer />
+            </BrowserRouter>
         </div>
     );
 }
