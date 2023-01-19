aa3c80eda7b5578967c70bcaa48006f37500b772
DaniAr <daniarangop93@gmail.com>
2023-01-19 16:58:23 +0100

    login update

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { login, reset } from "../../features/auth/authSlice";
+import { login, reset } from "../../features/auth/authSlice"
 import { notification } from "antd";
 import { useNavigate } from "react-router-dom";
 
