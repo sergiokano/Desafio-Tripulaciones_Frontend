236413d7966ece81e337f94f6936857b73a7c033
Alex Jimenez <alexjimenezada@gmail.com>
2023-01-20 21:29:54 +0100

    Deleted Header and Footer in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
-import TestMap from "./components/TestMap/TestMap";
 import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import TestMap from "./components/TestMap/TestMap";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Header />
                 <main>
                     <Routes>
                         <Route path="/" element={<Home />}></Route>
                         <Route path="/testmap" element={<TestMap />}></Route>
                         <Route path="/loginUser" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
                     </Routes>
                 </main>
-                <Footer />
             </BrowserRouter>
         </div>
     );
