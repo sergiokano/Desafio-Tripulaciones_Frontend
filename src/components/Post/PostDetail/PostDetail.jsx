7b826b043261f1972ed831dda0e5a3f276e39a7d
sergiokano <sergiocano.design@gmail.com>
2023-01-24 19:34:49 +0100

    post detail - comments css done, next step fix userID

--- src/components/Post/PostDetail/PostDetail.jsx
+++ src/components/Post/PostDetail/PostDetail.jsx
@@ -7,7 +7,7 @@ import Polifilo from "../../../assets/pngs/polifilo.png";
 import { getById } from "../../../features/posts/postsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import Header from "../../Header/Header";
-import { MdOutlinePlace } from "react-icons/md";
+import { MdOutlineModeComment, MdOutlinePlace } from "react-icons/md";
 
 const PostDetail = () => {
   const { post } = useSelector((state) => state.posts);
@@ -38,29 +38,40 @@ const PostDetail = () => {
             <span className="ml-6 mr-6 mt-7 flex flex-row left-6 font-bold leading-6 items-center">
               <MdOutlinePlace className="text-20 mr-0.5" />
               <span className="text-[11px]">{post.address}</span>
-              <span className="text-[11px] relative ml-auto ">
+              <span className="text-[11px]  relative ml-auto ">
                 Nº Validaciones: {post.verification.length}
               </span>
             </span>
-            {/* Cuadrado general comentarios */}
-            {/* primer coment */}
-            <div className="flex flex-col mt-12 border border-neutral-4 bg-neutral-1 ">
-              <div>
-                <div className="flex flex-col  ml-6 ">
-                  <span>{post.userId}</span>
-                  <span className="relative font-text leading-6 text-[15px] ">
+            <div className="ml-6 mt-3 text-[13px] text-blue-2 font-bold">
+              Daños espacio público
+            </div>
+
+            <div className="flex flex-col mt-5 border border-neutral-2 bg-neutral-1 ">
+              <div className=" text-[14px]">
+                <div className="flex flex-col  ml-6 mt-3.5 mb-3.5">
+                  <div className="flex flex-row space-x-2 items-center">
+                    <MdOutlineModeComment />
+                    <span className="font-bold text-[11px]">{post.userId}</span>
+                  </div>
+                  <span className="relative font-text leading-6">
                     {post.description}
                   </span>
                 </div>
-                {/* segundo comentario etc. */}
-                <div className="border border-t-neutral-4">
-                  <div className="flex flex-col  ml-6  ">
-                    <span>{post.userId}</span>
-                    <span className="relative font-text leading-6 text-[15px] ">
+             
+                {post.comments.length > 0 ? (
+                <div className="border border-t-neutral-2 ">
+                  <div className="flex flex-col  ml-6 mt-3.5 mb-3.5">
+                    <div className="flex flex-row space-x-2 items-center">
+                      <MdOutlineModeComment />
+                      <span className="font-bold text-[11px]">
+                        {post.userId}
+                      </span>
+                    </div>
+                    <span className="relative font-text leading-6 ">
                       {post.description}
                     </span>
                   </div>
-                </div>
+                </div>) : null}
               </div>
             </div>
           </div>
