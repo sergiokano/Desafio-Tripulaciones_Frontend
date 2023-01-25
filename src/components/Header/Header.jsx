b41c8281f9ae97915a5183d168bd922e1bf27bef
DaniAr <daniarangop93@gmail.com>
2023-01-25 19:58:37 +0100

    to profile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { FaRegUserCircle } from "react-icons/fa";
+import { useNavigate } from "react-router";
 import DropdownIcon from "../../assets/svgs/DropdownIcon.svg";
 import "./Header.scss";
 
 const Header = () => {
+    const navigate = useNavigate();
     return (
         <header className="flex items-center justify-between w-screen p-6 text-white bg-orange-1">
             <div className="flex gap-3 items-center">
@@ -12,7 +14,9 @@ const Header = () => {
             </div>
             <div>
                 <span>
-                    <FaRegUserCircle className="text-[22px]" />
+                    <FaRegUserCircle className="text-[22px]" 
+                    style={{ cursor: "pointer" }}
+                    onClick={() => navigate("/profile")}/>
                 </span>
             </div>
         </header>
