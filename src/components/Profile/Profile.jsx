dbc9519d7e5a97528d67ee99bb56dbe058eadbe6
Alex Jimenez <alexjimenezada@gmail.com>
2023-01-26 20:28:42 +0100

    Change Profile design

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
+import { FaRegUserCircle } from "react-icons/fa";
+import { MdArrowBackIosNew } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 import ProfileActivity from "./ProfileActivity/ProfileActivity";
 import ProfileView from "./ProfileView/ProfileView";
 
 const Profile = () => {
     const [isView, setIsView] = useState(true);
+    const navigate = useNavigate();
+    const user = JSON.parse(localStorage.getItem("user"));
     return (
         <div className="flex justify-center w-screen h-screen overflow-x-hidden">
             <div className="flex flex-col items-center w-full">
-                <div className="w-5/6 flex bg-[#D9D9D9] rounded-[12px] leading-7 text-[17px] my-7">
+                <div className="relative w-5/6 flex justify-center items-center mt-2 py-5">
+                    <span className="font-semibold flex justfy-center gap-2 items-center">
+                        <FaRegUserCircle className="text-2xl" />
+                        {user.user.username}
+                    </span>
+                    <button
+                        onClick={() => navigate(-1)}
+                        className="absolute left-0 text-neutral-5 border border-neutral-5 rounded-full p-2 "
+                    >
+                        <MdArrowBackIosNew className="text-[24px]" />
+                    </button>
+                </div>
+                <div className="w-5/6 flex bg-[#D9D9D9] rounded-[12px] leading-7 text-[17px] mt-2 mb-7">
                     <span
                         className={`flex-1 py-2 text-center font-semibold rounded-[12px] ${
                             isView
--- src/components/Profile/ProfileView/ProfileView.jsx
+++ src/components/Profile/ProfileView/ProfileView.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Location from "../../../assets/location.svg";
-import Vector from "../../../assets/Vector.svg";
+import IncidenceBlack from "../../../assets/svgs/IncidenceBlack.svg";
 import { getInfo } from "../../../features/auth/authSlice";
 import "./ProfileView.scss";
 
 const ProfileView = () => {
     const { user } = useSelector((state) => state.auth);
     const { posts } = useSelector((state) => state.posts);
+    const navigate = useNavigate();
 
     const dispatch = useDispatch();
     const API_URL = process.env.REACT_APP_API_URL;
@@ -21,41 +23,64 @@ const ProfileView = () => {
         return <p>Loading...</p>;
     }
 
-    const post = user.user.postIds.map((post) => {
+    const post = user.user.postIds.map((post, i) => {
+        const normalizeAddress = (address) => {
+            return address
+                .split(" ")
+                .map((s) => s[0] + s.slice(1, s.length).toLowerCase())
+                .join(" ");
+        };
+
         return (
-            <div>
-                <div key={post._id}>
+            <div
+                key={i}
+                className="w-5/6 border border-neutral-5 rounded-[12px]"
+            >
+                <div>
+                    {post.image_path && (
+                        <div className="rounded-t-[12px]">
+                            <img
+                                className="w-full rounded-t-[12px]"
+                                src={API_URL + "/" + post.image_path}
+                                alt=""
+                            />
+                        </div>
+                    )}
                     <div>
-                        {post.image_path && (
-                            <div>
-                                <img
-                                    className="w-full"
-                                    src={API_URL + "/" + post.image_path}
-                                    alt=""
-                                />
-                            </div>
-                        )}
-                        <div>
-                            <div className="vector-container">
-                                <img
-                                    src={Vector}
-                                    alt="Logo"
-                                    className="logo-img"
-                                />
-                                <h3 className="indidence-title">
-                                    {post.incidence}
-                                </h3>
-                            </div>
-                            <div className="location-container">
-                                <img
-                                    src={Location}
-                                    alt="Logo"
-                                    className="loc-img"
-                                />
-                                <p className="description-title">
-                                    {post.address}
-                                </p>
-                            </div>
+                        <div
+                            className={`vector-container ${
+                                post.image_path ? "" : "rounded-t-[12px]"
+                            }`}
+                        >
+                            <img
+                                src={IncidenceBlack}
+                                alt="Logo"
+                                className="logo-img ml-5"
+                            />
+                            <span className="px-2 py-4">{post.incidence}</span>
+                        </div>
+                        <div className="location-container rounded-b-[12px]">
+                            <img
+                                src={Location}
+                                alt="Logo"
+                                className="loc-img ml-6"
+                            />
+                            <span className="description-title px-2 py-4 w-full">
+                                {post.address
+                                    ? normalizeAddress(post.address)
+                                    : ""}
+                            </span>
+                        </div>
+                        <div className="w-full rounded-b-[12px] flex justify-center py-2">
+                            <button
+                                type="button"
+                                onClick={() => {
+                                    navigate(`/post/${post._id}`);
+                                }}
+                                className="text-white text-[17px] disabled:opacity-70 bg-black flex justify-center items-center gap-2.5 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 rounded-[12px] py-2.5 px-6"
+                            >
+                                <span>Ver</span>
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -63,13 +88,7 @@ const ProfileView = () => {
         );
     });
 
-    return (
-        <div>
-            <p className="incidence-title">{user.user.firstName}</p>
-
-            {post}
-        </div>
-    );
+    return <div className="flex flex-col items-center gap-2 pb-6">{post}</div>;
 };
 
 export default ProfileView;
